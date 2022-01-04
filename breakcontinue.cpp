6d3210be371d1870a3ff17bdc14489311c331ece
Vivek <86347471+Vivek1105@users.noreply.github.com>
2022-01-05 00:00:33 +0530

    Add files via upload

---
+++ breakcontinue.cpp
@@ -0,0 +1,31 @@
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//     for(int i=0; i<10; i++)
+//     {
+//         cout<<i<<endl;
+//         if(i==5){
+//             break;
+//         }
+        
+//     }
+//     return 0;
+// }
+
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    for(int i=0; i<10; i++)
+    {
+        
+        if(i==5){
+            continue;
+        }
+        cout<<i<<endl;
+        
+    }
+    return 0;
+}
