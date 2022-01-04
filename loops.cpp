44ad3e79309d86e566794a55f33acf72ded74cf6
Vivek <86347471+Vivek1105@users.noreply.github.com>
2022-01-05 00:00:14 +0530

    Add files via upload

---
+++ loops.cpp
@@ -0,0 +1,40 @@
+// for loop
+// #include<iostream>
+// using namespace std;
+
+// int main()
+// {
+//     for (int i=1; i<=40; i++)
+//     cout<<i<<endl;
+//   return 0;
+// }
+
+
+// while lopp
+// #include<iostream>
+// using namespace std;
+
+// int main()
+// {
+//     int i=1;
+//     while(i<=50){
+//     cout<<i<<endl;
+//     i++;
+//     }
+//     return 0;
+// }
+
+// do while loop
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int i=1;
+    do{
+    cout<<i<<endl;
+    i++;
+    }while(false);
+    
+    return 0;
+}
